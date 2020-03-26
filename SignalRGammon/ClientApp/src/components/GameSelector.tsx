3b9cb6d9248ff9886cb0a090a49e70560e6cf3a5
Matt DeKrey <mattdekrey@gmail.com>
2020-03-26 07:10:24 -0500

    Add Checkers link w/ styling

--- SignalRGammon/ClientApp/src/components/GameSelector.tsx
+++ SignalRGammon/ClientApp/src/components/GameSelector.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './GameSelector.css';
 
 export function GameSelector(props: { children?: never }) {
     return (
         <>
-            <div>
+            <div className="games-list">
                 <Link to="/backgammon">
                     <img src={require('./backgammon/backgammon.svg')} title="Backgammon" alt="Backgammon" />
                     Backgammon
+                </Link>
+                <Link to="/checkers">
+                    <img src={require('./checkers/empty-chessboard.svg')} title="Checkers" alt="Checkers" />
+                    Checkers
                 </Link>
             </div>
             <hr/>
--- SignalRGammon/ClientApp/src/components/LicensesComponent.tsx
+++ SignalRGammon/ClientApp/src/components/LicensesComponent.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 
+const delapouite = { author: "Delapouite", authorUrl: "http://delapouite.com" };
+const ccBy3 = { license: "CC BY 3.0", licenseUrl: "http://creativecommons.org/licenses/by/3.0/" };
+const gameIcons = { source: "game-icons.net", sourceUrl: "https://game-icons.net/" };
+
+const licenses = [
+    { title: "Backgammon icon", ...delapouite, ...ccBy3, ...gameIcons },
+    { title: "Empty chessboard icon", ...delapouite, ...ccBy3, ...gameIcons },
+]
+
 export function LicensesComponent(_: { children?: never }) {
     return (
         <ul>
-            <li>
-                {" "}Backgammon icon by{" "}
-                <a href="http://delapouite.com" rel="author">Delapouite</a>
-                {" "}under{" "}
-                <a href="http://creativecommons.org/licenses/by/3.0/" rel="license">CC BY 3.0</a>
-                {" "}via{" "}
-                <a href="https://game-icons.net/">game-icons.net</a>
-            </li>
+            {licenses.map(entry =>
+                <li key={entry.title}>
+                    {entry.title}{" "}by{" "}
+                    <a href={entry.authorUrl} rel="author">{entry.author}</a>
+                    {" "}under{" "}
+                    <a href={entry.licenseUrl} rel="license">{entry.license}</a>
+                    {" "}via{" "}
+                    <a href={entry.sourceUrl}>{entry.source}</a>
+                </li>
+            )}
         </ul>
     );
 }
